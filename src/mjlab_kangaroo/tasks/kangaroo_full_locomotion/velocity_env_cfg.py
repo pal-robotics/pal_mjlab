43f5dc4a189c91e408e461c374882c2ddf801ccb
Fabio Amadio <fabioamadio93@gmail.com>
2025-10-17 18:07:38 +0200

    Fix command lin_vel_x range

--- src/mjlab_kangaroo/tasks/kangaroo_full_locomotion/velocity_env_cfg.py
+++ src/mjlab_kangaroo/tasks/kangaroo_full_locomotion/velocity_env_cfg.py
@@ -76,7 +76,7 @@ class CommandsCfg:
         heading_control_stiffness=0.5,
         debug_vis=True,
         ranges=mdp.UniformVelocityCommandCfg.Ranges(
-            lin_vel_x=(0.5, 1.0),
+            lin_vel_x=(-1.0, 1.0),
             lin_vel_y=(-0.5, 0.5),
             ang_vel_z=(-1.0, 1.0),
             heading=(-math.pi, math.pi),
