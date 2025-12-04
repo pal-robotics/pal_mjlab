99020d295cd1064ce53f95bc49e85cd48e3cc2d4
sofieblankers <sofie.blankers@pal-robotics.com>
2025-12-04 10:18:16 +0100

    weights for lift reward

--- src/pal_mjlab/tasks/reaching_tiago/reaching_env_cfg.py
+++ src/pal_mjlab/tasks/reaching_tiago/reaching_env_cfg.py
@@ -164,7 +164,7 @@ def make_reaching_env_cfg() -> ManagerBasedRlEnvCfg:
     rewards = {
         "lift_task": RewardTermCfg(
             func=mdp.staged_position_reward,
-            weight=-0.5,
+            weight=-1.0,
             params={
                 "command_name": "lift_height",
                 "object_name": "cube",
@@ -180,14 +180,14 @@ def make_reaching_env_cfg() -> ManagerBasedRlEnvCfg:
         # "dof_pos_limits": RewardTermCfg(func=mdp.joint_pos_limits, 
         #     weight=-1.0
         #     ),
-        "action_rate_l2": RewardTermCfg(
-            func=mdp.action_rate_l2_louis,
-            weight=-0.0001,
-            params={
-                "asset_cfg": SceneEntityCfg(
-                    "robot", joint_names=(".*",)),  # Set per-robot.
-            },
-        ),
+        # "action_rate_l2": RewardTermCfg(
+        #     func=mdp.action_rate_l2_louis,
+        #     weight=-0.0001,
+        #     params={
+        #         "asset_cfg": SceneEntityCfg(
+        #             "robot", joint_names=(".*",)),  # Set per-robot.
+        #     },
+        # ),
         # "joint_acc_l2": RewardTermCfg(
         # func=mdp.joint_acc_l2,
         # weight=-0.1e-4,
