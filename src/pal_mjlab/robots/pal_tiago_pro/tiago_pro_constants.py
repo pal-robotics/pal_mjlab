131a20a2fee5b329cb5b39a1c9ca2951166a2c83
sofieblankers <sofie.blankers@pal-robotics.com>
2025-12-10 14:17:49 +0100

    weights grippers

--- src/pal_mjlab/robots/pal_tiago_pro/tiago_pro_constants.py
+++ src/pal_mjlab/robots/pal_tiago_pro/tiago_pro_constants.py
@@ -289,8 +289,8 @@ GRIPPER_ACTUATOR_CFG = BuiltinPositionActuatorCfg(
         "gripper_right_outer_finger_left_joint",),
     effort_limit=GRIPPER_EFFORT_LIMIT,
     armature=2*GRIPPER_ARMATURE,    
-    stiffness=500.0, #GRIPPER_STIFFNESS,
-    damping= 4.0, #GRIPPER_DAMPING,
+    stiffness=2000.0, #GRIPPER_STIFFNESS,
+    damping= 30.0, #GRIPPER_DAMPING,
 )
 
 
