9f9c4976fc9da687b586434a745f09c57560a47b
sofieblankers <sofie.blankers@pal-robotics.com>
2025-12-16 14:58:31 +0100

    remove reused rewards

--- src/pal_mjlab/tasks/reaching_tiago/mdp/rewards.py
+++ src/pal_mjlab/tasks/reaching_tiago/mdp/rewards.py
@@ -18,73 +18,6 @@ if TYPE_CHECKING:
 
 _DEFAULT_ASSET_CFG = SceneEntityCfg("robot")
 
-
-def position_command_error(
-    env: ManagerBasedRlEnv,
-    command_name: str,
-    site_name: str,
-    asset_cfg: SceneEntityCfg = _DEFAULT_ASSET_CFG,
-) -> torch.Tensor:
-    asset: Entity = env.scene[asset_cfg.name]
-    command = env.command_manager.get_term(command_name)
-
-    des_pos_b = command.command[:, :3]
-
-    root_pos_w = asset.data.site_pos_w[:, 0]  # Root site position
-    root_quat_w = asset.data.site_quat_w[:, 0]  # Root site quaternion
-
-    # Transform position: p_w = p_root + R_root * p_b
-    pos_rotated = quat_mul(
-        quat_mul(
-            root_quat_w,
-            torch.cat(
-                [torch.zeros(env.num_envs, 1, device=env.device), des_pos_b], dim=1
-            ),
-        ),
-        quat_conjugate(root_quat_w),
-    )[:, 1:]  # Extract xyz from quaternion product
-    des_pos_w = root_pos_w + pos_rotated
-
-    current_site_pos_w = asset.data.site_pos_w[:, asset.site_names.index(site_name)]
-
-    pos_error = current_site_pos_w - des_pos_w
-
-    return torch.norm(pos_error, dim=1)
-
-
-def position_command_error_tanh(
-    env: ManagerBasedRlEnv,
-    command_name: str,
-    site_name: str,
-    std: float,
-    asset_cfg: SceneEntityCfg = _DEFAULT_ASSET_CFG,
-) -> torch.Tensor:
-    asset: Entity = env.scene[asset_cfg.name]
-    command = env.command_manager.get_term(command_name)
-
-    des_pos_b = command.command[:, :3]
-
-    root_pos_w = asset.data.site_pos_w[:, 0]  # Root site position
-    root_quat_w = asset.data.site_quat_w[:, 0]  # Root site quaternion
-
-    pos_rotated = quat_mul(
-        quat_mul(
-            root_quat_w,
-            torch.cat(
-                [torch.zeros(env.num_envs, 1, device=env.device), des_pos_b], dim=1
-            ),
-        ),
-        quat_conjugate(root_quat_w),
-    )[:, 1:]  # Extract xyz from quaternion product
-    des_pos_w = root_pos_w + pos_rotated
-
-    current_site_pos_w = asset.data.site_pos_w[:, asset.site_names.index(site_name)]
-
-    pos_error = current_site_pos_w - des_pos_w
-    distance = torch.norm(pos_error, dim=1)
-
-    return 1 - torch.tanh(distance / std)
-
 def orientation_command_error(
     env: "ManagerBasedRlEnv",
     command_name: str,
@@ -146,28 +79,3 @@ def joint_velocity_hinge_penalty(
   joint_vel = robot.data.joint_vel[:, asset_cfg.joint_ids]
   excess = (joint_vel.abs() - max_vel).clamp_min(0.0)
   return (excess**2).sum(dim=-1)
-
-
-class action_rate_l2_louis:
-    def __init__(self, cfg: RewardTermCfg, env: ManagerBasedRlEnv):
-        asset: Entity = env.scene[cfg.params["asset_cfg"].name]
-
-        _, joint_names = asset.find_joints(
-            cfg.params["asset_cfg"].joint_names,
-        )
-        self._joint_ids = [
-            asset.actuator_names.index(jname)
-            for jname in joint_names
-            if jname in asset.actuator_names
-        ]
-
-    def __call__(
-        self, env: ManagerBasedRlEnv, asset_cfg: SceneEntityCfg
-    ) -> torch.Tensor:
-        return torch.sum(
-            torch.square(
-                env.action_manager.action[:, self._joint_ids]
-                - env.action_manager.prev_action[:, self._joint_ids]
-            ),
-            dim=1,
-        )
