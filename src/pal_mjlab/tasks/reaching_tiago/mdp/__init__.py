20ea81a2f39b637071497cf40a918ed4266cfd56
sofieblankers <sofie.blankers@pal-robotics.com>
2025-12-16 15:25:32 +0100

    remove duplicate to reaching kang

--- src/pal_mjlab/tasks/reaching_tiago/mdp/__init__.py
+++ src/pal_mjlab/tasks/reaching_tiago/mdp/__init__.py
@@ -1,5 +1,3 @@
 from pal_mjlab.tasks.reaching.mdp import *  # noqa: F401, F403
 
-from .commands import *  # noqa: F401, F403
 from .rewards import *  # noqa: F401, F403
-from .observations import *  # noqa: F401, F403
