df59c13ca53cd2d7d693f5f4fb890db50d8f5d93
ManuelActisCassa <manuel.actis.cassa@gmail.com>
2026-02-19 15:56:25 +0100

    Increase pos reward weights

--- src/pal_mjlab/tasks/reaching/reaching_env_cfg.py
+++ src/pal_mjlab/tasks/reaching/reaching_env_cfg.py
@@ -168,7 +168,7 @@ def make_reaching_env_cfg() -> ManagerBasedRlEnvCfg:
     rewards = {
         "pos_left": RewardTermCfg(
             func=mdp.position_command_error,
-            weight=-2.0,
+            weight=-4.0,
             params={
                 "site_name": "ee_left",
                 "command_name": "pose_command_left",
@@ -176,7 +176,7 @@ def make_reaching_env_cfg() -> ManagerBasedRlEnvCfg:
         ),
         "pos_left_fine_grained": RewardTermCfg(
             func=mdp.position_command_error_tanh,
-            weight=3.0,
+            weight=10.0,
             params={
                 "site_name": "ee_left",
                 "command_name": "pose_command_left",
@@ -193,7 +193,7 @@ def make_reaching_env_cfg() -> ManagerBasedRlEnvCfg:
         ),
         "pos_right": RewardTermCfg(
             func=mdp.position_command_error,
-            weight=-2.0,
+            weight=-4.0,
             params={
                 "site_name": "ee_right",
                 "command_name": "pose_command_right",
@@ -201,7 +201,7 @@ def make_reaching_env_cfg() -> ManagerBasedRlEnvCfg:
         ),
         "pos_right_fine_grained": RewardTermCfg(
             func=mdp.position_command_error_tanh,
-            weight=3.0,
+            weight=10.0,
             params={
                 "site_name": "ee_right",
                 "command_name": "pose_command_right",
