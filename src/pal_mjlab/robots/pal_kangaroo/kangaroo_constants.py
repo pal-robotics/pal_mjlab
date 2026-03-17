88c232fa335f71aeacf30931507881082628c502
Óscar Martínez <oscar.martinez@pal-robotics.com>
2026-03-17 09:21:47 +0100

    (fix) Stiffer foot contact

--- src/pal_mjlab/robots/pal_kangaroo/kangaroo_constants.py
+++ src/pal_mjlab/robots/pal_kangaroo/kangaroo_constants.py
@@ -258,6 +258,8 @@ FULL_COLLISION = CollisionCfg(
   condim={_FOOT_REGEX: 3, ".*_collision": 1},
   priority={_FOOT_REGEX: 1},
   friction={_FOOT_REGEX: (0.6,)},
+  solimp={_FOOT_REGEX: (0.999, 0.9999, 0.001,)},
+  solref={_FOOT_REGEX: (0.01, 1.0,)}
 )
 
 ##
