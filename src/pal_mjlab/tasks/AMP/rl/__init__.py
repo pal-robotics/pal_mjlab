6ec928664d61ab3e689afd37fc055044e0b28762
ManuelActisCassa <manuel.actis.cassa@gmail.com>
2026-04-15 14:43:43 +0200

    Kangaroo AMP first commit

---
+++ src/pal_mjlab/tasks/AMP/rl/__init__.py
@@ -0,0 +1,7 @@
+from pal_mjlab.tasks.AMP.rl.runner import (
+  AmpOnPolicyRunner as AmpOnPolicyRunner,
+)
+
+from pal_mjlab.tasks.AMP.rl.networks import (
+  DiscriminatorCfg as DiscriminatorCfg,
+)
