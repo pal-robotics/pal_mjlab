b6165f0ac064575520fa47253db433a227649923
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2026-05-11 13:45:21 +0200

    Register the lower body task

--- src/pal_mjlab/tasks/velocity/kangaroo/__init__.py
+++ src/pal_mjlab/tasks/velocity/kangaroo/__init__.py
@@ -7,6 +7,7 @@ from .env_cfgs import (
   pal_kangaroo_grippers_rough_env_cfg,
   pal_kangaroo_hands_flat_env_cfg,
   pal_kangaroo_hands_rough_env_cfg,
+  pal_kangaroo_lower_body_flat_env_cfg,
   pal_kangaroo_rough_env_cfg,
 )
 from .rl_cfg import pal_kangaroo_ppo_runner_cfg
@@ -23,6 +24,14 @@ register_mjlab_task(
   task_id="Mjlab-Velocity-Flat-Pal-Kangaroo",
   env_cfg=pal_kangaroo_flat_env_cfg(),
   play_env_cfg=pal_kangaroo_flat_env_cfg(play=True),
+  rl_cfg=pal_kangaroo_ppo_runner_cfg(),
+  runner_cls=VelocityOnPolicyRunner,
+)
+
+register_mjlab_task(
+  task_id="Mjlab-Velocity-Flat-Pal-Kangaroo-Lower-Body",
+  env_cfg=pal_kangaroo_lower_body_flat_env_cfg(),
+  play_env_cfg=pal_kangaroo_lower_body_flat_env_cfg(play=True),
   rl_cfg=pal_kangaroo_ppo_runner_cfg(),
   runner_cls=VelocityOnPolicyRunner,
 )
