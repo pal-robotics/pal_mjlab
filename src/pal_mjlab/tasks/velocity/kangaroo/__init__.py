3294aadb805c9289cf09537c228a5eae235a1189
Óscar Martínez <oscar.martinez@pal-robotics.com>
2026-07-14 15:14:44 +0200

    Adding custom PAL rough terrain config
    Replaces the default with all kinds of terrains with the tested pebbles,
    random spread and stairs curriculum + adapted rewards better suited for
    rough low vel

--- src/pal_mjlab/tasks/velocity/kangaroo/__init__.py
+++ src/pal_mjlab/tasks/velocity/kangaroo/__init__.py
@@ -44,6 +44,14 @@ register_mjlab_task(
 )
 
 register_mjlab_task(
+  task_id="Mjlab-Velocity-Rough-Pal-Kangaroo",
+  env_cfg=pal_kangaroo_rough_env_cfg(),
+  play_env_cfg=pal_kangaroo_rough_env_cfg(play=True),
+  rl_cfg=pal_kangaroo_ppo_runner_cfg(),
+  runner_cls=VelocityOnPolicyRunner,
+)
+
+register_mjlab_task(
   task_id="Mjlab-Velocity-Rough-Pal-Kangaroo-Grippers",
   env_cfg=pal_kangaroo_grippers_rough_env_cfg(),
   play_env_cfg=pal_kangaroo_grippers_rough_env_cfg(play=True),
