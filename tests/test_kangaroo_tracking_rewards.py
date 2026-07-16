8d2ede78b846133e5c204fb04da31f159f18c3dd
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2026-07-16 18:31:42 +0200

    Fix formatting

--- tests/test_kangaroo_tracking_rewards.py
+++ tests/test_kangaroo_tracking_rewards.py
@@ -133,7 +133,9 @@ def test_tracking_all_feet_air_time_reward(mock_env):
   env.scene["contact_sensor"].data.current_air_time[:, 0] += 0.2
   env.scene["contact_sensor"].data.current_air_time[:, 1] += 0.4
 
-  value = pal_mjlab_r.all_feet_air_time(env, sensor_name="contact_sensor", threshold=0.5)
+  value = pal_mjlab_r.all_feet_air_time(
+    env, sensor_name="contact_sensor", threshold=0.5
+  )
 
   test_name = "Tracking - feet air time (both below threshold)"
 
@@ -144,7 +146,9 @@ def test_tracking_all_feet_air_time_reward(mock_env):
 
   # One air time below threshold
 
-  value = pal_mjlab_r.all_feet_air_time(env, sensor_name="contact_sensor", threshold=0.3)
+  value = pal_mjlab_r.all_feet_air_time(
+    env, sensor_name="contact_sensor", threshold=0.3
+  )
 
   test_name = "Tracking - feet air time (one below threshold)"
 
@@ -155,7 +159,9 @@ def test_tracking_all_feet_air_time_reward(mock_env):
 
   # Both air times above threshold
 
-  value = pal_mjlab_r.all_feet_air_time(env, sensor_name="contact_sensor", threshold=0.1)
+  value = pal_mjlab_r.all_feet_air_time(
+    env, sensor_name="contact_sensor", threshold=0.1
+  )
 
   test_name = "Tracking - feet air time (one below threshold)"
 
