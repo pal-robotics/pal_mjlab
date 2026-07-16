ddf4c76bb8cd9947730d9acafd958a42565bba94
ManuelActisCassa <manuel.actis.cassa@gmail.com>
2026-07-16 17:08:55 +0200

    Format test fixes

--- tests/test_kangaroo_tracking_rewards.py
+++ tests/test_kangaroo_tracking_rewards.py
@@ -5,12 +5,12 @@ import pytest
 import torch
 from pal_mjlab.tasks.tracking.mdp import rewards as pal_mjlab_r
 
-
 # -------------------------------------#
 #                                      #
 #              UTILITIES               #
 #                                      #
 # -------------------------------------#
+
 
 @pytest.fixture
 def mock_env():
@@ -32,6 +32,7 @@ def mock_asset_cfg():
 
   return asset_cfg
 
+
 # -------------------------------------#
 #                                      #
 #            Dummy helpers             #
@@ -43,10 +44,14 @@ class command_manager:
   def __init__(self, env):
     dummy_motion_command = Mock()
 
-    dummy_motion_command.anchor_lin_vel_w = torch.zeros((env.num_envs,3), device=env.device)
+    dummy_motion_command.anchor_lin_vel_w = torch.zeros(
+      (env.num_envs, 3), device=env.device
+    )
     dummy_motion_command.anchor_lin_vel_w[:, 2] += 2.0
 
-    dummy_motion_command.robot_anchor_lin_vel_w = torch.zeros((env.num_envs,3), device=env.device)
+    dummy_motion_command.robot_anchor_lin_vel_w = torch.zeros(
+      (env.num_envs, 3), device=env.device
+    )
 
     self.active_terms = {
       "motion": dummy_motion_command,
@@ -97,56 +102,50 @@ def test_motion_global_anchor_velocity_z_error_exp_reward(mock_env):
   )
 
 
-def test_tracking_feet_air_time_reward (mock_env):
-    env = mock_env
+def test_tracking_feet_air_time_reward(mock_env):
+  env = mock_env
 
-    env.scene["contact_sensor"] = Mock()
+  env.scene["contact_sensor"] = Mock()
 
-    # Both air times below threshold
+  # Both air times below threshold
 
-    env.scene["contact_sensor"].data.current_air_time = torch.zeros((env.num_envs, 2,), device= env.device)
-    env.scene["contact_sensor"].data.current_air_time[:, 0] += 0.2
-    env.scene["contact_sensor"].data.current_air_time[:, 1] += 0.4
+  env.scene["contact_sensor"].data.current_air_time = torch.zeros(
+    (
+      env.num_envs,
+      2,
+    ),
+    device=env.device,
+  )
+  env.scene["contact_sensor"].data.current_air_time[:, 0] += 0.2
+  env.scene["contact_sensor"].data.current_air_time[:, 1] += 0.4
 
-    value = pal_mjlab_r.feet_air_time(
-        env, sensor_name= "contact_sensor", threshold= 0.5
-    )
+  value = pal_mjlab_r.feet_air_time(env, sensor_name="contact_sensor", threshold=0.5)
 
-    test_name = "Tracking _ feet air time (both below threshold)"
+  test_name = "Tracking _ feet air time (both below threshold)"
 
-    assert value.shape == (env.num_envs,), tensor_shape_error_message(
-        test_name, (env.num_envs,), value.shape
-    )
-    assert value[0] == pytest.approx(0.0, abs=1e-6), (
-        tensor_value_error_message(test_name)
-    )
-    
-    # One air time below threshold
+  assert value.shape == (env.num_envs,), tensor_shape_error_message(
+    test_name, (env.num_envs,), value.shape
+  )
+  assert value[0] == pytest.approx(0.0, abs=1e-6), tensor_value_error_message(test_name)
 
-    value = pal_mjlab_r.feet_air_time(
-        env, sensor_name= "contact_sensor", threshold= 0.3
-    )
+  # One air time below threshold
 
-    test_name = "Tracking _ feet air time (one below threshold)"
+  value = pal_mjlab_r.feet_air_time(env, sensor_name="contact_sensor", threshold=0.3)
 
-    assert value.shape == (env.num_envs,), tensor_shape_error_message(
-        test_name, (env.num_envs,), value.shape
-    )
-    assert value[0] == pytest.approx(0.0, abs=1e-6), (
-        tensor_value_error_message(test_name)
-    )
+  test_name = "Tracking _ feet air time (one below threshold)"
 
-    # Both air times above threshold
+  assert value.shape == (env.num_envs,), tensor_shape_error_message(
+    test_name, (env.num_envs,), value.shape
+  )
+  assert value[0] == pytest.approx(0.0, abs=1e-6), tensor_value_error_message(test_name)
 
-    value = pal_mjlab_r.feet_air_time(
-        env, sensor_name= "contact_sensor", threshold= 0.1
-    )
+  # Both air times above threshold
 
-    test_name = "Tracking _ feet air time (one below threshold)"
+  value = pal_mjlab_r.feet_air_time(env, sensor_name="contact_sensor", threshold=0.1)
 
-    assert value.shape == (env.num_envs,), tensor_shape_error_message(
-        test_name, (env.num_envs,), value.shape
-    )
-    assert value[0] == pytest.approx(0.3, abs=1e-6), (
-        tensor_value_error_message(test_name)
-    )
+  test_name = "Tracking _ feet air time (one below threshold)"
+
+  assert value.shape == (env.num_envs,), tensor_shape_error_message(
+    test_name, (env.num_envs,), value.shape
+  )
+  assert value[0] == pytest.approx(0.3, abs=1e-6), tensor_value_error_message(test_name)
