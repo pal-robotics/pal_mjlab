1c010e7fe19e8f680aea1dcebd7c3b8a61165bb5
ManuelActisCassa <manuel.actis.cassa@gmail.com>
2026-07-27 15:28:15 +0200

    Safer joint position at reset for pelvis

--- src/pal_mjlab/tasks/velocity/kangaroo/env_cfgs.py
+++ src/pal_mjlab/tasks/velocity/kangaroo/env_cfgs.py
@@ -744,7 +744,7 @@ def pal_kangaroo_leg_control_only_flat_env_cfg(play: bool = False) -> ManagerBas
     func=mdp.reset_static_joints_by_offset,
     mode="reset",
     params={
-      "position_range": (-0.1, 0.1),
+      "position_range": (-0.05, 0.05),
       "velocity_range": (0.0, 0.0),
       "asset_cfg": SceneEntityCfg("robot", joint_names=(r"pelvis_1.*",)),
     },
@@ -754,7 +754,7 @@ def pal_kangaroo_leg_control_only_flat_env_cfg(play: bool = False) -> ManagerBas
     func=mdp.reset_static_joints_by_offset,
     mode="reset",
     params={
-      "position_range": (-1.0, 1.0),
+      "position_range": (-0.7, 0.7),
       "velocity_range": (0.0, 0.0),
       "asset_cfg": SceneEntityCfg("robot", joint_names=(r"pelvis_2.*",)),
     },
